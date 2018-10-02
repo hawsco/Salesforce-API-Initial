5bfc1a81b7afc5f678599b8d6355acb435290fa0
monika ravi <monikar@hawsco.com>
2018-10-02 13:33:59 -0700

    callback url updated for heroku

--- Salesforce/access.js
+++ Salesforce/access.js
@@ -6,7 +6,7 @@ var conn = new jsforce.Connection({
       loginUrl: 'https://test.salesforce.com/',
       clientId: '3MVG9MHOv_bskkhQKsOfEdjGIWrWajLB2pf_QqbXZQZhEXGBNlP2NxNzBStxHH4augZU7vuG.FchltxVAIVrd',
       clientSecret: '2261574018905486495',
-      redirectUri: 'http://localhost:3000/'
+      redirectUri: 'http://localhost:3000/' || 'https://gentle-reef-30466.herokuapp.com/'
     }
   });
  
